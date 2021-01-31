f33063e1245ef8039ad0e571a6aa0eed66a5843c
TurgutYilmazturk <turgut.yilmazturk@mapaglobal.com>
2021-01-31 20:42:54 +0300

    commit-first

---
+++ analysis/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+from django.views import generic
+from . import models
+from django.shortcuts import get_object_or_404
+from django.contrib import messages
+import praw
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from praw.models import MoreComments
+
+
+reddit = praw.Reddit(
+     client_id="J5ECOUqkljDYvQ",
+     client_secret="HWN03vk8ykTOEm3zTCTfJShlLsnHrw",
+     user_agent="www.turgutcemyilmazturk.com /tubikcan",
+     username="tubikcan",
+     password="redditicinpass",
+ )
+
+sia = SentimentIntensityAnalyzer()
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
+
+# Create your views here.
+
+class AnalysisView(generic.CreateView):
+
+    fields=('topic',)
+    model=models.Analysis
+
+    def form_valid(self,form):
+        # if(AnalysisCheck()):
+            self.object = form.save(commit=False)
+            self.object.analysis_positive=1
+            self.object.analysis_neutral=1
+            self.object.analysis_negative=1
+            self.object.user=self.request.user
+            self.object.save()
+            return HttpResponse('<h1>YES</h1>')
+
+
+
+
+class HistoryView(generic.ListView):
+    context_object_name='h_analysis'
+    model=models.Analysis
+
+    def get_queryset(self):
+        return models.Analysis.objects.filter(user=self.request.user)
+
+def AnalysisCheck():
+    pass
