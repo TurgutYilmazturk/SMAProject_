2d812acd4e4442d77d981535b17b6a33722ab28b
TurgutYilmazturk <turgut.yilmazturk@mapaglobal.com>
2021-02-02 02:30:15 +0300

    Changes in DB-Frontend and Analysis.

--- analysis/models.py
+++ analysis/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 import requests
 from django.utils.text import slugify
 import misaka
-from django import template
+from django import template,forms
 
 
 # Create your models here.
@@ -15,8 +15,15 @@ User=get_user_model()
 
 class Analysis(models.Model):
     user=models.ForeignKey(User,related_name='analysis',on_delete=models.CASCADE)
-    topic=models.TextField()
+    topic=models.CharField(('Topic to be searched in Reddit'),max_length=300)
     analysis_positive=models.IntegerField(blank=False, null=False)
     analysis_negative=models.IntegerField(blank=False,null=False)
     analysis_neutral=models.IntegerField(blank=False,null=False)
     created_at=models.DateTimeField(auto_now=True)
+    CHOICES1=[(1,'1'),(2,'2'),(3,'3'),(4,'4'),(5,'5'),(6,'6'),(7,'7'),(8,'8'),(9,'9'),(10,'10'),(20,20),(50,50),(100,100)]
+    CHOICES2=[('all','all'),('day','day'),('hour','hour'),('month','month'),('week','week'),('year','year')]
+    limit = models.IntegerField('Count of "Top Posts" to be searched in Reddit.Default value=all',choices=CHOICES1)
+    time_filter=models.CharField('Reddit API time interval',choices=CHOICES2,max_length=300)
+
+    class Meta():
+         ordering = ['-created_at']
--- analysis/views.py
+++ analysis/views.py
@@ -12,13 +12,16 @@ from praw.models import MoreComments
 from django.contrib.auth import get_user_model
 from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import redirect
+from django import template
+from prawcore import NotFound
 
+
+register = template.Library()
 reddit = praw.Reddit(
-     client_id="J5ECOUqkljDYvQ",
-     client_secret="HWN03vk8ykTOEm3zTCTfJShlLsnHrw",
-     user_agent="www.turgutcemyilmazturk.com /tubikcan",
-     username="tubikcan",
-     password="redditicinpass",
+     client_id="bZHFNA6gZjvG_Q",
+     client_secret="1l-pgLeHaR-D66hrm4m58DZHpiZBkQ",
+     user_agent="www.turgutcemyilmazturk.com by /tubikcan",
+
  )
 
 sia = SentimentIntensityAnalyzer()
@@ -27,18 +30,32 @@ sia = SentimentIntensityAnalyzer()
 
 class AnalysisView(generic.CreateView):
 
-    fields=('topic',)
+    fields=('topic','limit','time_filter')
     model=models.Analysis
 
     def form_valid(self,form):
-        # if(AnalysisCheck()):
+
             self.object = form.save(commit=False)
-            self.object.analysis_positive=1
-            self.object.analysis_neutral=1
-            self.object.analysis_negative=1
-            self.object.user=self.request.user
-            self.object.save()
-            return HttpResponseRedirect("results")
+            global reddit
+            top_posts = reddit.subreddit(self.object.topic).top(self.object.time_filter, limit=self.object.limit)
+            try:
+                result_dict=AnalysisDone(top_posts)
+            except NotFound:
+                return HttpResponse("<h1>Hatalı<h1>")
+
+            if(result_dict['positive']==0 and result_dict['negative']==0 and result_dict['neutral'] <5):
+                return HttpResponseRedirect("<h1>Hatalı<h1>")
+
+            else:
+                self.object.user=self.request.user
+                self.object.analysis_neutral=result_dict['neutral']
+                self.object.analysis_negative=result_dict['negative']
+                self.object.analysis_positive=result_dict['positive']
+                self.object.save()
+                return HttpResponseRedirect("results")
+
+
+
 
 
 
@@ -53,5 +70,57 @@ class HistoryView(generic.ListView):
     def get_queryset(self):
         return models.Analysis.objects.filter(user=self.request.user)
 
-def AnalysisCheck():
-    pass
+
+def AnalysisDone(top_posts):
+    for submission in top_posts:
+        sub_entries_nltk = {'negative': 0, 'positive' : 0, 'neutral' : 0}
+        nltk_sentiment(submission.title, sub_entries_nltk)
+        submission_comm = reddit.submission(id=submission.id)
+
+        for count, top_level_comment in enumerate(submission_comm.comments):
+            count_comm = 0
+            try :
+                nltk_sentiment(top_level_comment.body, sub_entries_nltk)
+                replies_of(top_level_comment,
+                           count_comm,
+                           sub_entries_nltk)
+            except:
+                continue
+
+        return sub_entries_nltk
+
+
+def nltk_sentiment(review, sub_entries_nltk):
+    vs = sia.polarity_scores(review)
+    if not vs['neg'] > 0.05:
+        if vs['pos'] - vs['neg'] > 0:
+            sub_entries_nltk['positive'] = sub_entries_nltk['positive'] + 1
+            return 'Positive'
+        else:
+            sub_entries_nltk['neutral'] = sub_entries_nltk['neutral'] + 1
+            return 'Neutral'
+
+    elif not vs['pos'] > 0.05:
+        if vs['pos'] - vs['neg'] <= 0:
+            sub_entries_nltk['negative'] = sub_entries_nltk['negative'] + 1
+            return 'Negative'
+        else:
+            sub_entries_nltk['neutral'] = sub_entries_nltk['neutral'] + 1
+            return 'Neutral'
+    else:
+        sub_entries_nltk['neutral'] = sub_entries_nltk['neutral'] + 1
+        return 'Neutral'
+
+
+def replies_of(top_level_comment, count_comment, sub_entries_textblob, sub_entries_nltk):
+    if len(top_level_comment.replies) == 0:
+        count_comment = 0
+        return
+    else:
+        for num, comment in enumerate(top_level_comment.replies):
+            try:
+                count_comment += 1
+                nltk_sentiment(comment.body, sub_entries_nltk)
+            except:
+                continue
+            replies_of(comment, count_comment, sub_entries_textblob,sub_entries_nltk)
